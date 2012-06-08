0b1181623e189c5513378aab900e8c4aa0a17dad
Rafeequl <rafeequl@veritrans.co.id>
2012-06-08 18:39:29 +0700

    more tabs to spaces

--- lib/config.php
+++ lib/config.php
@@ -1,16 +1,16 @@
 <?php
-	// Change with your merchant hash key
-	define('MERCHANT_ID', '10');
-	define('MERCHANT_HASH_KEY', 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz0123456789012345678901');
-	
-	// Configuration 
-	define('REQUEST_KEY_URL', 'http://localhost/~vcool/weblink/comodityRegist.txt');
-	define('PAYMENT_REDIRECT_URL', 'http://122.208.206.170:80/web1/deviceCheck.action');
-	
-	// Settlement method:
-	define('SETTLEMENT_TYPE_CARD', '01');
-	
-	// Flag: Sales and Sales Credit, 0: only 1 credit. If not specified, 0
-	define('CARD_CAPTURE_FLAG', '1');
-	
+  // Change with your merchant hash key
+  define('MERCHANT_ID', '10');
+  define('MERCHANT_HASH_KEY', 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz0123456789012345678901');
+  
+  // Configuration 
+  define('REQUEST_KEY_URL', 'http://localhost/~vcool/weblink/comodityRegist.txt');
+  define('PAYMENT_REDIRECT_URL', 'http://122.208.206.170:80/web1/deviceCheck.action');
+  
+  // Settlement method:
+  define('SETTLEMENT_TYPE_CARD', '01');
+  
+  // Flag: Sales and Sales Credit, 0: only 1 credit. If not specified, 0
+  define('CARD_CAPTURE_FLAG', '1');
+  
 ?>
--- lib/hash_generator.php
+++ lib/hash_generator.php
@@ -1,34 +1,34 @@
 <?php
 
 /**
- * HashGenerator class
- *
- * Generate using SHA-512
- */
+* HashGenerator class
+*
+* Generate using SHA-512
+*/
 
 class HashGenerator {
 
-    /**
-     * Generate hash value from string
-     * @param merchantID
-     * @param settlementmethod - how to settlement （Null or Blank → '00'）
-     * @param orderID
-     * @param amount - Total Amount
-     * @return hash value
-     */
-    public function generate($merchantID, $settlementmethod, $orderID, $amount) {
+  /**
+  * Generate hash value from string
+  * @param merchantID
+  * @param settlementmethod - how to settlement （Null or Blank → '00'）
+  * @param orderID
+  * @param amount - Total Amount
+  * @return hash value
+  */
+  public function generate($merchantID, $settlementmethod, $orderID, $amount) {
 
-        $ctx = hash_init('sha512');
+    $ctx  = hash_init('sha512');
 
-        $str = MERCHANT_HASH_KEY .
-                "," . $merchantID .
-                "," . ((is_null($settlementmethod) || strlen($settlementmethod) == 0) ? '00' : $settlementmethod) .
-                "," . $orderID .
-                "," . $amount;
-        hash_update($ctx, $str);
-        $hash = hash_final($ctx, true);
-        return bin2hex($hash);
-    }
+    $str  = MERCHANT_HASH_KEY .
+      "," . $merchantID .
+      "," . ((is_null($settlementmethod) || strlen($settlementmethod) == 0) ? '00' : $settlementmethod) .
+      "," . $orderID .
+      "," . $amount;
+    hash_update($ctx, $str);
+    $hash = hash_final($ctx, true);
+    return bin2hex($hash);
+  }
 
 }
 
