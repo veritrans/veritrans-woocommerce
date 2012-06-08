1840eb5b44737b2e7dd707f78fff0791b014405b
Rafeequl <rafeequl@veritrans.co.id>
2012-06-08 18:29:23 +0700

    initial commit

---
+++ lib/hash_generator.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * HashGenerator class
+ *
+ * Generate using SHA-512
+ */
+
+class HashGenerator {
+
+    /**
+     * Generate hash value from string
+     * @param merchantID
+     * @param settlementmethod - how to settlement （Null or Blank → '00'）
+     * @param orderID
+     * @param amount - Total Amount
+     * @return hash value
+     */
+    public function generate($merchantID, $settlementmethod, $orderID, $amount) {
+
+        $ctx = hash_init('sha512');
+
+        $str = MERCHANT_HASH_KEY .
+                "," . $merchantID .
+                "," . ((is_null($settlementmethod) || strlen($settlementmethod) == 0) ? '00' : $settlementmethod) .
+                "," . $orderID .
+                "," . $amount;
+        hash_update($ctx, $str);
+        $hash = hash_final($ctx, true);
+        return bin2hex($hash);
+    }
+
+}
+
+?>
---
+++ lib/request.php
@@ -0,0 +1,34 @@
+<?php
+/**
+* Request class for performing HTTP Request
+*/
+
+class Request
+{
+	private function request($method, $url, $params)
+	{
+		
+	}
+	
+	public function get($url, $params)
+	{
+		
+	}
+	
+	public function post($url, $params)
+	{
+		
+	}
+	
+	public function update($url, $params)
+	{
+		
+	}
+	
+	public function delete($url, $params)
+	{
+		
+	}
+}
+
+?>
