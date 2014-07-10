4cb8483bf7837e52ceb58294a568ff899c25e22d
febryantonius <febry.antonius07@gmail.com>
2014-07-10 11:26:34 +0700

    Subtree merged in cork

---
+++ lib/veritrans/Veritrans/Config.php
@@ -0,0 +1,19 @@
+<?php
+
+class Veritrans_Config {
+
+  public static $serverKey;
+  public static $apiVersion = 2;
+  public static $isProduction = false;
+  public static $is3ds = false;
+  public static $isSanitized = false;
+
+  const SANDBOX_BASE_URL = 'https://api.sandbox.veritrans.co.id/v2';
+  const PRODUCTION_BASE_URL = 'https://api.veritrans.co.id/v2';
+
+  public static function getBaseUrl()
+  {
+    return Veritrans_Config::$isProduction ?
+        Veritrans_Config::PRODUCTION_BASE_URL : Veritrans_Config::SANDBOX_BASE_URL;
+  }
+}
---
+++ lib/veritrans/Veritrans/VtDirect.php
@@ -0,0 +1,32 @@
+<?php
+
+class Veritrans_VtDirect {
+
+  public static function charge($params)
+  {
+    $payloads = array(
+        'payment_type' => 'credit_card'
+      );
+
+    if (array_key_exists('item_details', $params)) {
+      $gross_amount = 0;
+      foreach ($params['item_details'] as $item) {
+        $gross_amount += $item['quantity'] * $item['price'];
+      }
+      $payloads['transaction_details']['gross_amount'] = $gross_amount;
+    }
+
+    $payloads = array_replace_recursive($payloads, $params);
+
+    if (Veritrans_Config::$isSanitized) {
+      Veritrans_Sanitizer::jsonRequest($payloads);
+    }
+
+    $result = Veritrans_ApiRequestor::post(
+        Veritrans_Config::getBaseUrl() . '/charge',
+        Veritrans_Config::$serverKey,
+        $payloads);
+
+    return $result;
+  }
+}
