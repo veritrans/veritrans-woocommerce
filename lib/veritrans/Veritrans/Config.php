4f949d652b07bed37ca99999869c5dc732a6880c
Rizda Dwi Prasetya <rizdaprasetya@users.noreply.github.com>
2017-03-09 10:48:35 +0700

    Update api endpoint

--- lib/veritrans/Veritrans/Config.php
+++ lib/veritrans/Veritrans/Config.php
@@ -8,8 +8,10 @@ class Veritrans_Config {
   public static $is3ds = false;
   public static $isSanitized = false;
 
-  const SANDBOX_BASE_URL = 'https://api.sandbox.veritrans.co.id/v2';
-  const PRODUCTION_BASE_URL = 'https://api.veritrans.co.id/v2';
+  const SANDBOX_BASE_URL = 'https://api.sandbox.midtrans.com/v2';
+  const PRODUCTION_BASE_URL = 'https://api.midtrans.com/v2';
+  const SNAP_SANDBOX_BASE_URL = 'https://app.sandbox.midtrans.com/snap/v1';
+  const SNAP_PRODUCTION_BASE_URL = 'https://app.midtrans.com/snap/v1';
 
   public static function getBaseUrl()
   {
